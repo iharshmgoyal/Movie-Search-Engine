df5fa88eab09a7bd6c7c63752cf145bf699b73f6
Tnluser <goyalharsh7827@gmail.com>
2022-02-01 00:50:21 +0530

    Movie Search Engine - Login Activity Intent and some design change in sample RV MainActivity

--- app/src/main/java/com/codinggeekers/moviesearchengine/LoginActivity.java
+++ app/src/main/java/com/codinggeekers/moviesearchengine/LoginActivity.java
@@ -44,6 +44,10 @@ public class LoginActivity extends AppCompatActivity {
                     public void onComplete(@NonNull Task<AuthResult> task) {
                         if (task.isSuccessful()) {
                             Toast.makeText(LoginActivity.this, "User Login Success!!!", Toast.LENGTH_SHORT).show();
+                            startActivity(new Intent(LoginActivity.this, MainActivity.class));
+                        }
+                        else {
+                            Toast.makeText(LoginActivity.this, task.getException().getMessage(), Toast.LENGTH_SHORT).show();
                         }
                     }
                 });
